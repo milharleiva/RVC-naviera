0cefd6e2c72ce3b75029ebb66ed99739a716a90b
milhar leiva <milhar.leiva935@gmail.com>
2024-10-20 20:02:07 -0300

    storage 2
    test

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,14 +1,5 @@
-import { defineStorage } from "@aws-amplify/backend";
+import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-    
-    name : 'amplify-RVC-storage',
-    access: (allow) => ({
-        "product-images/*": [
-            allow.guest.to(["read"]),
-            allow.authenticated.to(["read"]),
-            allow.groups(["admins"]).to (["read", "write", "delete"]),
-        ]
-    })
-
+  name: 'amplifyTeamDrive'
 });
