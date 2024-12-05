9f816be40f343b66b95bb5ac137a7485da8841d5
milhar leiva <milhar.leiva935@gmail.com>
2024-12-05 00:56:58 -0300

    ver anuncios

--- src/app/dashboard/anuncios/page.tsx
+++ src/app/dashboard/anuncios/page.tsx
@@ -1,5 +1,12 @@
+import db from "@/lib/db"
 
-export default function Anuncios() {
+export  default async function Anuncios() {
+
+    const anuncios = await db.anuncio.findMany()
+
+    console.log(anuncios)
+
+
   return (
     <div>
       <h1>anuncios</h1>
