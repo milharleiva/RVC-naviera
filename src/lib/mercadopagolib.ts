f87230318ad87d723da5c5ded91febb4268cfbf3
milhar leiva <milhar.leiva935@gmail.com>
2024-12-09 21:29:02 -0300

    commit

--- src/lib/mercadopagolib.ts
+++ src/lib/mercadopagolib.ts
@@ -1,7 +1,7 @@
 import { MercadoPagoConfig,} from 'mercadopago';
 
 
-const mercadopago = new MercadoPagoConfig({ accessToken: process.env.MERCADOPAGO_ACCESS_TOKEN! });
+const mercadopago = new MercadoPagoConfig({ accessToken: process.env.MP_ACCESS_TOKEN! });
 
 export { mercadopago };
 
